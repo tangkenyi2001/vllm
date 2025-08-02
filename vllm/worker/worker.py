f84c50a0288d46bd1d656d1424e9d8fd0fec74ce
tangkenyi2001 <tangkenyi2001@gmail.com>
2025-08-02 18:32:12 +0800

    vllm diagram

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -10,6 +10,7 @@ import torch
 import torch.distributed
 
 import vllm.envs as envs
+
 from vllm.attention.layer import Attention
 from vllm.config import VllmConfig, get_layers_from_vllm_config
 from vllm.device_allocator.cumem import CuMemAllocator
