ea623879906d46d1a1971ce3cb41eca095c2098c
tangkenyi2001 <tangkenyi2001@gmail.com>
2025-12-03 09:29:56 +0000

    refactor: Remove redundant abstract.py and use core Executor

--- vllm/worker_controller/async_llm.py
+++ vllm/worker_controller/async_llm.py
@@ -39,7 +39,7 @@ from vllm.v1.engine.output_processor import (OutputProcessor,
                                              RequestOutputCollector)
 from vllm.v1.engine.parallel_sampling import ParentRequest
 from vllm.v1.engine.processor import Processor
-from vllm.worker_controller.abstract import Executor
+from vllm.v1.executor.abstract import Executor
 from vllm.v1.metrics.loggers import StatLoggerFactory, StatLoggerManager
 from vllm.v1.metrics.prometheus import shutdown_prometheus
 from vllm.v1.metrics.stats import IterationStats
